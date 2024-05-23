3df01e44230e8ad1f773785eafaae6f6c9a65d55
md-maheen-billah <md.maheen.billah@icloud.com>
2024-05-23 02:16:28 +0200

    created an api to perform delete functionality to remove a user

--- index.js
+++ index.js
@@ -55,7 +55,14 @@ async function run() {
       res.send(result);
     });
 
-    // menu related api
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // menu related apis
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
